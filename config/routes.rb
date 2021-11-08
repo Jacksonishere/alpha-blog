490730539c25db0e1c84604f141193a0235bf802
Jacksonishere <jackson.lu02@myhunter.cuny.edu>
2021-11-07 19:18:46 -0500

    can create users and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #home page
   root 'pages#home'
+  #about page
   get 'about', to: 'pages#about'
+  #articles
   resources :articles
+  #users
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
