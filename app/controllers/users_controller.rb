6de0b7043778f0c5bb66f5a6ac9f05f0077fe7d4
Jacksonishere <jackson.lu02@myhunter.cuny.edu>
2021-11-07 19:37:44 -0500

    able to edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :edit, :update, :destroy]
+
   def new
     @user = User.new
+  end
+
+  def update
   end
 
   def create
@@ -13,7 +18,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User was updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
