ca8fd9e28abb56a49753468d1f801e716379a1ff
Jacksonishere <jackson.lu02@myhunter.cuny.edu>
2021-11-09 22:30:05 -0500

    admin can change category name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,13 @@
 class CategoriesController < ApplicationController
-  before_action :require_admin, only: [:new, :create]
+  before_action :require_admin, except: [:index, :show]
+  before_action :find_category, only: [:show, :edit, :update]
   
   def index
     @categories = Category.paginate(page: params[:page], per_page: 3).order('id DESC')
   end
 
   def show
-    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def new
@@ -23,15 +24,31 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category was updated successfully."
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
     params.require(:category).permit(:name)
   end
 
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
   def require_admin
     if !(logged_in? && current_user.admin?)
-      flash[:alert] = "You are not allowed to create a new category"
+      flash[:alert] = "You are not allowed to create/edit a new category"
       redirect_to categories_path
     end
   end
